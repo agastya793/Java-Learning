707830fb24111e1c875ae11947fa03f7cb4e16f2
agastya793 <agastya071@gmail.com>
2025-10-18 21:52:33 +0530

    continue with loops

---
+++ Topics- units/unit-7 control statement , math and string/unit-7/src/forEachLoop.java
@@ -0,0 +1,12 @@
+public class forEachLoop {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements are:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
+
+
---
+++ Topics- units/unit-7 control statement , math and string/unit-7/src/forLoop.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class forLoop {
+    public static void main(String[] args) {
+        for ( int j = 1; j <= 100 ; j++ ){
+            System.out.println(j + " ");
+        }
+    }
+}
