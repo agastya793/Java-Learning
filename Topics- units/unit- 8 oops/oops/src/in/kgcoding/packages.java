63abc32b7de6be4dc3aa05aed346b15e17bb7cbe
agastya793 <agastya071@gmail.com>
2025-10-21 23:49:19 +0530

    started loops now

---
+++ Topics- units/unit- 8 oops/oops/src/in/kgcoding/packages.java
@@ -0,0 +1,9 @@
+package in.kgcoding;
+
+import java.util.Scanner;
+
+    public class packages {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
---
+++ Topics- units/unit-7 control statement , math and string/unit-7/src/usefulOperation.java
@@ -0,0 +1,11 @@
+
+public class usefulOperation {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-87));
+        System.out.println(Math.ceil(5.07));
+        System.out.println(Math.floor(5.03));
+        System.out.println(Math.round(7.09));
+
+
+    }
+}
